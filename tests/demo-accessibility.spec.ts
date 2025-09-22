3c0d5679c4075fba31cc122c0d43a3d51f0abafa
Roselle <roselleroxtabuena@gmail.com>
2025-09-22 19:45:35 +0800

    Add accessibility tests and  todo app functionality

---
+++ tests/demo-accessibility.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect, type Page } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
+  await page.goto('https://your-site.com/');
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
